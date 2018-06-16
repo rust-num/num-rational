f0a6ad275d5faf226f93267c12dbaf63794fb3e7
Josh Stone <cuviper@gmail.com>
2018-06-15 22:35:10 -0700

    Add support for 128-bit integers

--- ci/test_full.sh
+++ ci/test_full.sh
@@ -4,6 +4,11 @@ set -ex
 
 echo Testing num-rational on rustc ${TRAVIS_RUST_VERSION}
 
+FEATURES="std bigint-std serde"
+if [[ "$TRAVIS_RUST_VERSION" =~ ^(nightly|beta|stable|1.26.0)$ ]]; then
+  FEATURES="$FEATURES i128"
+fi
+
 # num-rational should build and test everywhere.
 cargo build --verbose
 cargo test --verbose
@@ -13,7 +18,11 @@ cargo build --no-default-features
 cargo test --no-default-features
 
 # Each isolated feature should also work everywhere.
-for feature in bigint serde std; do
+for feature in $FEATURES; do
   cargo build --verbose --no-default-features --features="$feature"
   cargo test --verbose --no-default-features --features="$feature"
 done
+
+# test all supported features together
+cargo build --features="$FEATURES"
+cargo test --features="$FEATURES"
