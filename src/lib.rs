e10ca814e7b1f4d5fcc42ed5e7ceb39f8443ef5c
Max Blachman <blachmanmax@gmail.com>
2019-06-17 07:40:53 -0700

    Fix #49

--- src/lib.rs
+++ src/lib.rs
@@ -385,6 +385,9 @@ impl<T: Clone + Integer> Ord for Ratio<T> {
 
         // With equal numerators, the denominators can be inversely compared
         if self.numer == other.numer {
+            if T::is_zero(self.numer()) {
+                return cmp::Ordering::Equal;
+            }
             let ord = self.denom.cmp(&other.denom);
             return if self.numer < T::zero() {
                 ord
@@ -1446,6 +1449,9 @@ mod test {
 
         assert!(_0 >= _0 && _1 >= _1);
         assert!(_1 >= _0 && !(_0 >= _1));
+
+        let _0_2: Rational = Ratio::new_raw(0, 2);
+        assert_eq!(_0, _0_2);
     }
 
     #[test]
@@ -1538,7 +1544,7 @@ mod test {
 
     mod arith {
         use super::super::{Ratio, Rational};
-        use super::{_0, _1, _1_2, _2, _3_2, _NEG1_2, to_big};
+        use super::{to_big, _0, _1, _1_2, _2, _3_2, _NEG1_2};
         use traits::{CheckedAdd, CheckedDiv, CheckedMul, CheckedSub};
 
         #[test]
